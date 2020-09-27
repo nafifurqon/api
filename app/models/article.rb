bb519d728a83865424305d95b0f198d3c2816f52
nafifurqon <muhammadnafi98@gmail.com>
2020-09-27 16:01:31 +0700

    Implemented proper order articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
     validates_presence_of :title, :content
     validates :slug, presence: true, uniqueness: true
+
+    scope :recent, -> { order(created_at: :desc) }
 end
