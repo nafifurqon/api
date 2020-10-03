1fd5d51b28b3642a4bde6913a5830c75ada268a9
nafifurqon <muhammadnafi98@gmail.com>
2020-10-03 20:03:05 +0700

    Fixed allow to update only owned articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,7 @@ class Article < ApplicationRecord
     validates_presence_of :title, :content
     validates :slug, presence: true, uniqueness: true
 
+    belongs_to :user
+
     scope :recent, -> { order(created_at: :desc) }
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -147,7 +147,9 @@ describe ArticlesController do
     end
 
     describe '#update' do
-        let(:article){ create :article }
+        let(:user){ create :user }
+        let(:article){ create :article, user: user }
+        let(:access_token){ user.create_access_token }
 
         subject{ patch :update, params: { id: article.id } }
 
@@ -160,8 +162,15 @@ describe ArticlesController do
             it_behaves_like 'forbidden_requests'
         end
 
+        context 'when trying to update not owned article' do
+            let(:other_user) { create :user }
+            let(:other_article) { create :article, user: other_user }
+            subject{ patch :update, params: { id: other_article.id } }
+            before { request.headers['authorization'] = 'Invalid token' }
+            it_behaves_like 'forbidden_requests'
+        end
+
         context 'when authorized' do
-            let(:access_token){ create :access_token }
             before{ request.headers['authorization'] = "Bearer #{access_token.token}" }
 
             context 'when invalid parameters provided' do
